3f516d89d3adb9f734e586a1aec84cc5d9b48843
kiran <crackerplace@gmail.com>
2016-07-23 01:27:55 +0530

    Create FacilitiesExtraction.java

---
+++ FacilitiesExtraction.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+/**
+ * Not an optimized solution as complexity is O(N^2).Use trie to optimize it.
+ * Also for this programming problem,encapsulation is not adhered.
+ * 
+ * */
+ 
+class FacilitiesExtraction {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine();
+        List<String> facs = new ArrayList<>();
+        for(int i =0;i<N;i++){
+            facs.add(sc.nextLine());               
+        }
+        String text = sc.nextLine().toLowerCase();
+        List<String> matchedFacs = new ArrayList<>();
+        for(String fac:facs){
+            if(text.indexOf(fac.trim().toLowerCase())>0){
+            	matchedFacs.add(fac);
+            }
+        }
+        Collections.sort(matchedFacs);
+        for(String fac:matchedFacs){
+        	System.out.println(fac);
+        }
+        
+    }
+    
+}
